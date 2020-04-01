b4033e6bd5b247ab7a0ba7acbde08b54f2250e25
Rahul Pandey <hello@rkpandey.com>
2020-03-31 18:15:45 -0700

    Functional memory game

---
+++ app/src/main/java/edu/stanford/rkpandey/memorygame/MainActivity.kt
@@ -0,0 +1,93 @@
+package edu.stanford.rkpandey.memorygame
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.ImageButton
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+private const val TAG = "MainActivity"
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var buttons: List<ImageButton>
+    private lateinit var cards: List<MemoryCard>
+    private var indexOfSingleSelectedCard: Int? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val images = mutableListOf(R.drawable.ic_heart, R.drawable.ic_light, R.drawable.ic_plane, R.drawable.ic_smiley)
+        // Add each image twice so we can create pairs
+        images.addAll(images)
+        // Randomize the order of images
+        images.shuffle()
+
+        buttons = listOf(imageButton1, imageButton2, imageButton3, imageButton4, imageButton5,
+                imageButton6, imageButton7, imageButton8)
+
+        cards = buttons.indices.map { index ->
+            MemoryCard(images[index])
+        }
+
+        buttons.forEachIndexed { index, button ->
+            button.setOnClickListener {
+                Log.i(TAG, "button clicked!!")
+                // Update models
+                updateModels(index)
+                // Update the UI for the game
+                updateViews()
+            }
+        }
+    }
+
+    private fun updateViews() {
+        cards.forEachIndexed { index, card ->
+            val button = buttons[index]
+            if (card.isMatched) {
+                button.alpha = 0.1f
+            }
+            button.setImageResource(if (card.isFaceUp) card.identifier else R.drawable.ic_code)
+        }
+    }
+
+    private fun updateModels(position: Int) {
+        val card = cards[position]
+        // Error checking:
+        if (card.isFaceUp) {
+            Toast.makeText(this, "Invalid move!", Toast.LENGTH_SHORT).show()
+            return
+        }
+        // Three cases
+        // 0 cards previously flipped over => restore cards + flip over the selected card
+        // 1 card previously flipped over => flip over the selected card + check if the images match
+        // 2 cards previously flipped over => restore cards + flip over the selected card
+        if (indexOfSingleSelectedCard == null) {
+            // 0 or 2 selected cards previously
+            restoreCards()
+            indexOfSingleSelectedCard = position
+        } else {
+            // exactly 1 card was selected previously
+            checkForMatch(indexOfSingleSelectedCard!!, position)
+            indexOfSingleSelectedCard = null
+        }
+        card.isFaceUp = !card.isFaceUp
+    }
+
+    private fun restoreCards() {
+        for (card in cards) {
+            if (!card.isMatched) {
+                card.isFaceUp = false
+            }
+        }
+    }
+
+    private fun checkForMatch(position1: Int, position2: Int) {
+        if (cards[position1].identifier == cards[position2].identifier) {
+            Toast.makeText(this, "Match found!!", Toast.LENGTH_SHORT).show()
+            cards[position1].isMatched = true
+            cards[position2].isMatched = true
+        }
+    }
+}
---
+++ app/src/main/java/edu/stanford/rkpandey/memorygame/MemoryCard.kt
@@ -0,0 +1,3 @@
+package edu.stanford.rkpandey.memorygame
+
+data class MemoryCard(val identifier: Int, var isFaceUp: Boolean = false, var isMatched: Boolean = false)
